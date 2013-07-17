bcca75d215d24b7d1fb2ae76c3c98745a70d5551
Nan Monnand Deng <monnand@gmail.com>
2013-07-17 19:24:01 -0400

    new simulator.

---
+++ conn.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"net"
+)
+
+type APNSNotificaton struct {
+	command    uint8
+	id         uint32
+	expiry     uint32
+	tokenLen   uint16
+	devToken   []byte
+	payloadLen uint16
+	payload    []byte
+}
+
+func (self *APNSNotificaton) String() string {
+	return fmt.Sprintf("command=%v\nid=%v\nexpiry=%v\ntoken=%v\n,payload=%v\n",
+		self.command, self.id, self.expiry, self.devToken, string(self.payload))
+}
+
+type APNSResponse struct {
+	id     uint32
+	status uint8
+}
+
+type APNSConn struct {
+	conn net.Conn
+}
+
+func (self *APNSConn) Close() error {
+	return self.conn.Close()
+}
+
+func (self *APNSConn) ReadNotification() (notif *APNSNotificaton, err error) {
+	notif = new(APNSNotificaton)
+	conn := self.conn
+	err = binary.Read(conn, binary.BigEndian, &(notif.command))
+	if err != nil {
+		notif = nil
+		return
+	}
+
+	if notif.command == 1 {
+		err = binary.Read(conn, binary.BigEndian, &(notif.id))
+		if err != nil {
+			notif = nil
+			return
+		}
+		err = binary.Read(conn, binary.BigEndian, &(notif.expiry))
+		if err != nil {
+			notif = nil
+			return
+		}
+	} else if notif.command != 0 {
+		notif = nil
+		err = fmt.Errorf("Unkown Command")
+		return
+	}
+	err = binary.Read(conn, binary.BigEndian, &(notif.tokenLen))
+	if err != nil {
+		notif = nil
+		return
+	}
+
+	// devtoken
+	notif.devToken = make([]byte, notif.tokenLen)
+	n, err := io.ReadFull(self.conn, notif.devToken)
+	if err != nil {
+		notif = nil
+		return
+	}
+	if n != int(notif.tokenLen) {
+		notif = nil
+		err = fmt.Errorf("no enough data")
+		return
+	}
+
+	// payload size
+	err = binary.Read(conn, binary.BigEndian, &(notif.payloadLen))
+	if err != nil {
+		notif = nil
+		return
+	}
+
+	// payload
+	notif.payload = make([]byte, notif.payloadLen)
+	n, err = io.ReadFull(conn, notif.payload)
+	if err != nil {
+		notif = nil
+		return
+	}
+	if n != int(notif.payloadLen) {
+		notif = nil
+		err = fmt.Errorf("no enough data: payload")
+		return
+	}
+	return
+}
+
+func (self *APNSConn) Reply(status *APNSResponse) error {
+	var command uint8
+	command = 8
+	err := binary.Write(self.conn, binary.BigEndian, command)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(self.conn, binary.BigEndian, status.status)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(self.conn, binary.BigEndian, status.id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -3,46 +3,91 @@ package main
 import (
 	"crypto/rand"
 	"crypto/tls"
-	"encoding/binary"
 	"fmt"
+	"github.com/uniqush/log"
 	"io"
 	weakrand "math/rand"
 	"net"
+	"os"
 	"time"
 )
 
-func handleClient(conn net.Conn) {
-	defer conn.Close()
-	for {
-		var status uint8
-		notif, err := readNotification(conn)
-		if err != nil {
-			fmt.Printf("Got an error: %v\n", err)
-			break
-		}
-		// we don't need a good random generator. Even with mod is ok.
-		s := weakrand.Int() % 100
+type APNSProcessor struct {
+	MinProcessingTime time.Duration
+	MaxProcessingTime time.Duration
 
-		if s > 50 {
-			s -= 50
-			s /= 5
-		} else {
-			s = 0
-		}
-		status = uint8(s)
-		if status == uint8(0) {
-			if weakrand.Int()%5 != 0 {
-				fmt.Printf("[%v] Got a notifcation: %v. It is successfully processed but will not be replied. I am a bad fruit\n", time.Now(), notif)
-				continue
-			}
-		} else if status > uint8(8) {
-			status = 255
-			fmt.Printf("[%v] Drop this connection\n", time.Now())
+	Simulator APNSSimulator
+	Conn      *APNSConn
+	Log       log.Logger
+
+	sendChan chan *APNSResponse
+}
+
+func NewAPNSProcessor(conn *APNSConn, logger log.Logger) *APNSProcessor {
+	ret := new(APNSProcessor)
+	ret.Conn = conn
+	ret.Log = logger
+	ret.sendChan = make(chan *APNSResponse)
+	return ret
+}
+
+func (self *APNSProcessor) sendingResponses(start chan bool) {
+	<-start
+	for res := range self.sendChan {
+		err := self.Conn.Reply(res)
+		if err != nil {
 			return
 		}
-		fmt.Printf("[%v] Got a notifcation: %v. and the status is: %v\n", time.Now(), notif, status)
-		replyNotification(conn, status, notif.id)
 	}
+}
+
+func (self *APNSProcessor) Process() {
+	if self.sendChan == nil {
+		self.sendChan = make(chan *APNSResponse)
+	}
+	if self.Conn == nil {
+		return
+	}
+	if self.Simulator == nil {
+		self.Simulator = &APNSNormalSimulator{0, 0}
+	}
+
+	ch := make(chan bool)
+	self.sendingResponses(ch)
+	ch <- true
+
+	defer self.Conn.Close()
+
+	for {
+		notif, err := self.Conn.ReadNotification()
+		if err != nil {
+			self.Log.Errorf("Got an error: %v\n", err)
+			return
+		}
+		res, err := self.Simulator.Reply(notif)
+		if err != nil {
+			self.Log.Errorf("Got an error: %v\n", err)
+			return
+		}
+
+		waitTime := self.MaxProcessingTime
+		if self.MaxProcessingTime != self.MinProcessingTime {
+			t := weakrand.Int63n(self.MaxProcessingTime.Nanoseconds() - self.MinProcessingTime.Nanoseconds())
+			t += self.MinProcessingTime.Nanoseconds()
+			waitTime = time.Duration(t)
+		}
+		go func(res *APNSResponse, wait time.Duration) {
+			time.Sleep(wait)
+			self.sendChan <- res
+		}(res, waitTime)
+	}
+}
+
+func handleClient(conn net.Conn, writer io.Writer, level int) {
+	logger := log.NewLogger(writer, fmt.Sprintf("[%v]", conn.RemoteAddr().String()), level)
+	c := &APNSConn{conn}
+	proc := NewAPNSProcessor(c, logger)
+	proc.Process()
 }
 
 func main() {
@@ -63,12 +108,13 @@ func main() {
 		fmt.Printf("Listen: %v\n", err)
 		return
 	}
+
 	for {
 		client, err := conn.Accept()
 		if err != nil {
 			fmt.Printf("Accept Error: %v\n", err)
 		}
 		fmt.Printf("[%v] Received connection from %v\n", time.Now(), client.RemoteAddr())
-		go handleClient(client)
+		go handleClient(client, os.Stderr, log.LOGLEVEL_DEBUG)
 	}
 }
